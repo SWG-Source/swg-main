732593c6d80245d3317f52ea9e1b05af8334d333
Elour <alec.91@gmail.com>
2018-12-04 15:10:44 -0800

    Update build_java for const/final variables Multi can be updated with the same method as well, this clears up a few issues when compiling libraries that other scripts reference consts from

--- utils/build_java.sh
+++ utils/build_java.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
+WDIR="$(pwd)" #working dir (normally /home/swg/swg-main )
 
 destination="data/sku.0/sys.server/compiled/game"
 sourcepath="dsrc/sku.0/sys.server/compiled/game"
@@ -13,12 +14,26 @@ i=0
 current=0
 total=$(ls ${filenames[@]} | wc -l)
 
+declare -A items=() #this holds all of the scriptlib const things that need recompilation
+
 for filename in $filenames; do
     OFILENAME=${filename/$sourcepath/$destination}
     OFILENAME=${OFILENAME/java/class}
 
     if [[ -e $OFILENAME && $filename -nt $OFILENAME ]] || [ ! -e $OFILENAME ]; then
 	   result=$(${DIR}/build_java_single.sh $filename 2>&1)
+	   while read -r l; do
+	      l=${l/"public static final "/""}
+	      #echo "l is $l" #debug
+	      l=$(echo $l | tr -s ' ' | cut -d ' ' -f 2) #grab field 2 (TYPE NAME =)
+	      echo "got const? $l" #debug
+	      cd $WDIR/dsrc/sku.0/sys.server/compiled/game/script #cd to scripts, for grep of $k
+	      while read -r k; do
+	         echo "got other? $k" #debug
+	         items[$k]=1 #add $k to our list to compile (at the end of the script)
+	      done < <(grep -Rinwl . -e $l) #find all scripts using the variable "$l"
+	      cd $WDIR #cd back to working directory after grep
+	   done < <(git --git-dir=$WDIR/dsrc/.git --work-tree=$WDIR/dsrc diff origin/master -U0 | grep -o "public static final .*=") #find all 'public static final' declarations in our changed scripts vs origin/master
     fi
 
     if [[ ! -z $result ]]; then
@@ -30,6 +45,12 @@ for filename in $filenames; do
     i=$(( (i+1) %4 ))
     perc=$(bc -l <<< "scale=0; $current*100/$total")
     printf "\rCompiling java scripts: [${spinstr:$i:1}] $perc%%"
+done
+
+for item in "${!items[@]}"; do
+    item=${item//"./"/}
+    echo "library CONST - recompiling $item"
+    $WDIR/utils/build_java_single.sh $WDIR/dsrc/sku.0/sys.server/compiled/game/script/$item
 done
 
 echo ""
