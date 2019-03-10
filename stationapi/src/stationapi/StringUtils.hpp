d67e01031fd5975f620510a97443b35ec0eba438
Tekaoh <45337851+Tekaoh@users.noreply.github.com>
2019-03-10 11:48:26 -0500

    Remove stationapi/ from swg-main. It's getting its own repo.

--- stationapi/src/stationapi/StringUtils.hpp
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include <string>
-
-std::string FromWideString(const std::u16string& str);
-
-std::u16string ToWideString(const std::string& str);
--- stationapi/udplibrary/UdpLibrary.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Network_UdpLibrary_H
-#define _Network_UdpLibrary_H
-
-#include "UdpLibrary.hpp"
-
-#endif//_Network_UdpLibrary_H
