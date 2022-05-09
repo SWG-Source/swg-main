e682e199392c7bb8437bbaed598748125cb85dcd
Alec Harley <alec.91@gmail.com>
2022-05-08 19:38:52 -0700

    Add Planet CRC utility

---
+++ utils/build_planet_crc_string_tables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from os import path, makedirs
+from subprocess import PIPE, Popen
+
+inputfile = './dsrc/sku.0/sys.shared/compiled/game/misc/planet_crc_string_table.txt'
+ifffile = './data/sku.0/sys.shared/compiled/game/misc/planet_crc_string_table.iff'
+
+if not path.exists(path.dirname(ifffile)):
+	makedirs(path.dirname(ifffile))
+
+crc_call = ['./tools/buildCrcStringTable.pl', ifffile, inputfile]
+
+p = Popen(crc_call, stdin=PIPE, stdout=PIPE)
+
+p.communicate()
